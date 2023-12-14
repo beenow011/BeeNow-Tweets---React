24fc414848526cae9f6fe69efdb852157988f53b
beenow011 <abhinavnb11@gmail.com>
2023-12-14 15:10:04 +0530

    vercel upate

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BeeNow-Tweets---React",
+  // base: "/BeeNow-Tweets---React",
   plugins: [react()],
 })
